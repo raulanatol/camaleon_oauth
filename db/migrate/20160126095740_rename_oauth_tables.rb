55ecf711b3518ec5d4549b6fe10ad99d93105385
Raúl Anatol <raul@natol.es>
2016-01-27 21:50:39 +0000

    Tables update

--- db/migrate/20160126095740_rename_oauth_tables.rb
+++ db/migrate/20160126095740_rename_oauth_tables.rb
@@ -1,7 +1,7 @@
 class RenameOauthTables < ActiveRecord::Migration
   def change
-    rename_table :"#{PluginRoutes.static_system_info["db_prefix"]}oauth_access_grants", :oauth_access_grants
-    rename_table :"#{PluginRoutes.static_system_info["db_prefix"]}oauth_access_tokens", :oauth_access_tokens
-    rename_table :"#{PluginRoutes.static_system_info["db_prefix"]}oauth_applications", :oauth_applications
+    rename_table :"#{PluginRoutes.static_system_info["db_prefix"]}oauth_access_grants", :oauth_access_grants if table_exists?(:"#{PluginRoutes.static_system_info["db_prefix"]}oauth_access_grants")
+    rename_table :"#{PluginRoutes.static_system_info["db_prefix"]}oauth_access_tokens", :oauth_access_tokens if table_exists?(:"#{PluginRoutes.static_system_info["db_prefix"]}oauth_access_tokens")
+    rename_table :"#{PluginRoutes.static_system_info["db_prefix"]}oauth_applications", :oauth_applications if table_exists?(:"#{PluginRoutes.static_system_info["db_prefix"]}oauth_applications")
   end
 end
